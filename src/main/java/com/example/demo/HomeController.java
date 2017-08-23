89d28660abb64956b8083584e5de8e409f9856fe
RVSW317028DT <RVSW317028DT@MC.MONTGOMERYCOLLEGE.EDU>
2017-08-23 14:23:19 -0400

    learned to put basic security in a page

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+}
